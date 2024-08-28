4f84eedd7a8be3bdf0e58d4ab0aea27a802e62c5
Your Name <you@example.com>
2024-08-28 11:50:51 -0300

    Resolvendo conflitos

--- pages/manual.py
+++ pages/manual.py
@@ -1,20 +1,34 @@
 # Arquivo: pages/manual.py
 import streamlit as st
 import base64
+import os
 
 def main():
     st.title("Manual do Usuário")
 
-    # Caminho para o arquivo PDF na raiz do projeto
-    pdf_file = "manual.pdf"
+    # Caminho para o arquivo PDF
+    pdf_file = "manual.pdf"  # Certifique-se de que este é o nome correto do seu arquivo
+
+    # Verifica se o arquivo existe
+    if not os.path.isfile(pdf_file):
+        st.error(f"O arquivo {pdf_file} não foi encontrado.")
+        return
 
     # Lê o arquivo PDF
     with open(pdf_file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
     # Incorpora o PDF na página
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1200" height="1000" type="application/pdf"></iframe>'
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
+
+    # Adiciona um botão para download
+    st.download_button(
+        label="Baixar Manual PDF",
+        data=base64_pdf,
+        file_name="manual.pdf",
+        mime="application/pdf"
+    )
 
 
 # Adicionar o rodapé personalizado
